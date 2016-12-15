293b47691710ef1195b2df1f139d43e275ba81d5
yaoshun <yaoshun_d@qq.com>
2016-12-15 21:39:19 +0800

    cash

---
+++ demo-recursion/cash.js
@@ -0,0 +1,13 @@
+var store = {
+    nextId: 1,
+    cache: {},
+    add: function(fn){
+        if(!fn.id){
+            fn.id = store.nextId++;
+            return !!(store.cache[fn.id] = fn);
+        }
+    }
+};
+function ninja(){}
+
+console.log(store.add(ninja));
---
+++ demo-recursion/ninja3.js
@@ -0,0 +1,6 @@
+var ninja = function myNinja(){
+    console.log(ninja == myNinja); //true
+}
+ninja();
+
+typeof myNinja == "undefined" //true
