63a4c4b4bd8363671d24da620e2cecbb36e719e5
yaoshun <yaoshun_d@qq.com>
2016-12-18 18:07:33 +0800

    添加描述

--- demo-recursion/length3.js
+++ demo-recursion/length3.js
@@ -2,6 +2,12 @@
 function addMethod(object, name ,fn) {
     var old = object[name];
     object[name] = function() {
+        console.log('begin ======');
+        console.log(fn);
+        console.log(arguments);
+        console.log(fn.length == arguments.length);
+        console.log('end ======');
+
         if(fn.length == arguments.length)
             return fn.apply(this, arguments);
         else if (typeof old == 'function')    
